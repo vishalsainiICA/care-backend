636bcc429bcf008b8be6f873ea27293aa1ea39e3
vishalsainiICA <vishal.saini@icaimpetus.com>
2025-10-29 13:43:53 +0530

    Update main_pre.py
    chage frontent url in cors setup

--- main_pre.py
+++ main_pre.py
@@ -509,7 +509,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://emrpro.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
